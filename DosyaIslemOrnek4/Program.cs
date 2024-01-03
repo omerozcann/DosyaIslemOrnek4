666993fe4cc787a9282ad4f5e6ee6c124edaefa5
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2024-01-03 11:05:53 +0300

    Proje dosyası ekle.

---
+++ DosyaIslemOrnek4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DosyaIslemOrnek4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Metin türündeki verilerin okunması
+            String veri;
+            veri = File.ReadAllText("d:\\abc.txt");//abc.txt dosyasının tamamını oku
+            Console.WriteLine(veri);
+        }
+    }
+}
